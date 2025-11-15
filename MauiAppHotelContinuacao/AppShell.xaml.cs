e75ef38582f1bf799593687ff3b8ae23b571c8a2
denissvenancio <denis.sarraf@gmail.com>
2025-11-15 16:30:27 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotelContinuacao/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppHotelContinuacao
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
