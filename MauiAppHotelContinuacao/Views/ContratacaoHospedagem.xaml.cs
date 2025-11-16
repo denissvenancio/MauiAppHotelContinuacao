4fbd46de348aab557aba8d5b45533e0719ee9f2d
denissvenancio <denis.sarraf@gmail.com>
2025-11-15 21:28:57 -0300

    Primeira parte do Aplicativo

---
+++ MauiAppHotelContinuacao/Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppHotelContinuacao.Views;
+
+public partial class ContratacaoHospedagem : ContentPage
+{
+	public ContratacaoHospedagem()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAppHotelContinuacao/Views/HospedagemContratada.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppHotelContinuacao.Views;
+
+public partial class HospedagemContratada : ContentPage
+{
+	public HospedagemContratada()
+	{
+		InitializeComponent();
+	}
+}
